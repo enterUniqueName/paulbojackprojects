c2fe8833957d13a03c97bbe73e482587e031248d
Dave Steele <steelemediasolutions@gmail.com>
2021-10-22 12:23:54 -0400

    Added Cherish project

---
+++ projects/cherish/cherish.js
@@ -0,0 +1,134 @@
+//project specific scripts
+
+const camContainer = document.getElementById("cam_container");
+const blackscreen = document.getElementById("blackscreen");
+
+camContainer.style.opacity = "1";
+blackscreen.style.opacity = "1";
+
+function cardOne() {
+    let tl = gsap.timeline();
+    tl.to('#cherish-bright', {
+        duration: 3,
+        opacity: 1,
+    })
+    .to('#cherish-bright', {
+        duration: 3,
+        opacity: 0,
+    })
+    .to('#cherish-dark', {
+        duration: 3,
+        opacity: 1,
+    }, '<')
+    .to('#every', {
+        duration: 3,
+        opacity: 1,
+    }, '<')
+    .to('#moment', {
+        duration: 3,
+        opacity: 1,
+    })
+    .to('.card.one', {
+        opacity: 0,
+        duration: 3,
+    }, "+=3")
+    .to(blackscreen, {
+        opacity: 0,
+        duration: 0,
+    })
+    return tl;
+};
+function cardTwo() {
+    let tl = gsap.timeline();
+    tl
+    .set(blackscreen, {
+        opacity: 1,
+        duration: 0,
+    })
+    .to('#except-bright', {
+        duration: 3,
+        opacity: 1,
+    })
+    .to('#except-bright', {
+        duration: 3,
+        opacity: 0,
+    })
+    .to('#except-dark', {
+        duration: 3,
+        opacity: 1,
+    }, '<')
+    .to('#that', {
+        duration: 3,
+        opacity: 1,
+    }, '<')
+    .to('#one', {
+        duration: 3,
+        opacity: 1,
+    })
+    .to('.card.two', {
+        opacity: 0,
+        duration: 3,
+    }, "+=3")
+    .to(blackscreen, {
+        opacity: 0,
+        duration: 0,
+    })
+    return tl;
+};
+function cardThree() {
+    let tl = gsap.timeline();
+    tl
+    .set(blackscreen, {
+        opacity: 1,
+        duration: 0,
+    })
+    .to('#afew', {
+        duration: 2,
+        opacity: 1,
+    })
+    .to('#and', {
+        duration: 2,
+        opacity: 1,
+    })
+    .to('#comma', {
+        duration: 2,
+        opacity: 1,
+    })
+    .to('#maybe', {
+        duration: 2,
+        opacity: 1,
+    })
+    .to('#others', {
+        duration: 2,
+        opacity: 1,
+    })
+    .to('.card.three', {
+        opacity: 0,
+        duration: 3,
+    }, "+=2")
+    .to(blackscreen, {
+        opacity: 0,
+        duration: 0,
+    })
+    return tl;
+};
+function blackout() {
+    let tl = gsap.timeline();
+    tl
+    .set(blackscreen, {
+        opacity: 1,
+        duration: 0,
+    })
+    return tl;
+}
+
+
+//master fade function
+function master_animation() {
+    let mastertl = gsap.timeline();
+    mastertl
+    .add(cardOne())
+    .add(cardTwo(), '+=3')
+    .add(cardThree(), '+=3')
+    .add(blackout(), '+=3')
+}
